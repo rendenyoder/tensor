6aee88666cd058c0a6a3861428924e97ca3fbc67
Renden Yoder <rendeny1@gmail.com>
2021-10-30 19:26:31 -0700

    Refactored FashionMnist to extend ModelClient

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,10 +1,11 @@
-import os.path
+from abc import ABC
 from enum import Enum
 from random import randint
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+
+from cli import ModelClient
 
 
 class EnglishLabel(Enum):
@@ -23,59 +24,74 @@ class EnglishLabel(Enum):
     AnkleBoot = 9
 
 
-class FashionMnist:
+class FashionMnist(ModelClient, ABC):
     """
     Wrapping class for a model based on the fashion_mnist dataset.
     """
     def __init__(self):
-        fashion_mnist = keras.datasets.fashion_mnist
-        dataset = fashion_mnist.load_data()
-        (self.train_images, self.train_labels) = dataset[0]
-        (self.test_images, self.test_labels) = dataset[1]
-        self.model = self.load_model()
+        self.__dataset__ = keras.datasets.fashion_mnist.load_data()
+        super().__init__()
 
-    def load_model(self):
-        path = "models/fashion_mnist"
+    def __train_data__(self):
+        """
+        Gets the training data from the fashion_mnist dataset.
+        :return: tuple
+        """
+        return self.__dataset__[0]
 
-        if os.path.exists(path):
-            model = tf.keras.models.load_model(path)
-        else:
-            model = keras.Sequential([
-                keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu',
-                                    input_shape=(28, 28, 1)),
-                keras.layers.MaxPool2D(2, 2),
-                keras.layers.Flatten(input_shape=(28, 28)),
-                keras.layers.Dense(1024, activation=tf.nn.relu),
-                keras.layers.Dense(10, activation=tf.nn.softmax)
-            ])
+    def __test_data__(self):
+        """
+        Gets the testing data from the fashion_mnist dataset.
+        :return: tuple
+        """
+        return self.__dataset__[1]
 
-            self.train_images = self.train_images.reshape(60000, 28, 28, 1)
-            model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-            model.fit(self.train_images, self.train_labels, epochs=10)
+    def __model_from_path__(self):
+        """
+        Loads a pre-built model.
+        :return: model
+        """
+        return tf.keras.models.load_model('models/fashion_mnist/')
 
-            model.save("models/fashion_mnist")
+    def __build_and_save__(self):
+        """
+        Builds the model using the train data set.
+        :return: model
+        """
+        model = keras.Sequential([
+            keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu',
+                                input_shape=(28, 28, 1)),
+            keras.layers.MaxPool2D(2, 2),
+            keras.layers.Flatten(input_shape=(28, 28)),
+            keras.layers.Dense(1024, activation=tf.nn.relu),
+            keras.layers.Dense(10, activation=tf.nn.softmax)
+        ])
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
+        (train_data, train_labels) = self.__train_data__()
+        train_data = train_data.reshape(60000, 28, 28, 1)
+
+        model.fit(train_data, train_labels, epochs=10, verbose=1)
+        model.save("models/fashion_mnist")
         return model
 
     def predict(self, image_index):
+        """
+        Predicts the label for the test image at the given index.
+        :param image_index: Index of test image
+        """
+        (test_data, _) = self.__test_data__()
+
         plt.figure()
-        plt.imshow(self.test_images[image_index])
+        plt.imshow(test_data[image_index])
         plt.show()
 
-        test_image = self.test_images[image_index].reshape(1, 28, 28, 1)
-        classifications = self.model.predict(test_image)
-        classification = classifications[0]
-        max_val = np.max(classification)
-        most_probable = np.where(classification == max_val)
-        return EnglishLabel(most_probable[0][0])
-
-
-def main():
-    fashion_mnist = FashionMnist()
-    random = randint(0, 10000)
-    label = fashion_mnist.predict(random)
-    print(label)
+        test_image = test_data[image_index].reshape(1, 28, 28, 1)
+        prediction = self.model.predict(test_image)
+        max_index = prediction.argmax(axis=-1)
+        print(EnglishLabel(max_index[0]))
 
 
 if __name__ == "__main__":
-    main()
+    fashion_mnist = FashionMnist()
+    fashion_mnist.predict(randint(0, 10000))
